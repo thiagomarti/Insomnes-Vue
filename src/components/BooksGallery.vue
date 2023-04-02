f5c56337454ce6282771113e2a3041b67d683497
Thiago marti <octaltiticapo@gmail.com>
2023-04-02 20:30:10 -0300

    sume el buscador en antologias AYUDAME QUE NO ENTIENDO

---
+++ src/components/BooksGallery.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="gallery">
+      <h3>{{ title }}</h3>
+      <div class="galleryItems">
+        <div class="imageBox" v-for="(image, x) in items" :key="x" >
+          <img :src=image.src :alt=image.alt  />
+          <br />
+          <span>{{ image.description }}</span>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  /* eslint-disable */
+  export default {
+    name: 'BooksGallery',
+    props: ['items', 'title'], // Props recibir propiedades desde otros componentes.
+  }
+  
+  </script>
+  
+  <style scoped>
+  .imageBox {
+    margin-bottom:1.25rem;
+  }
+  .galleryItems {
+    columns:3;
+    gap:1.25rem;
+    padding-top:1.25rem;
+  }
+  </style>
--- src/views/AnthologiesView.vue
+++ src/views/AnthologiesView.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="bodygaleria" style="width: 100%; height: 100vh; background-color: blue;">
-    <p class="heading">NUESTROS TRABAJOS</p>
-    <div class="gallery-image">
+  <div class="search">
+    <label for="">Buscate algo</label> 
+    <input v-model="searchString" placeholder="search" class="search-bar">
+    <BooksGallery :items="filteredWorkers" :title="`Galería filtrada`" />
   </div>
-</div>
 </template>
 
 <script>
-
-
+// @ is an alias to /src
+import BooksGallery from '@/components/BooksGallery.vue'
+import assetsData from "@/assets/Libros.json";
 export default {
   name: 'AnthologiesView',
+  components: {
+    BooksGallery
+  },
+  data () 
+  {
+    return {
+      searchString: "",
+      data: assetsData
+    }     
+  },
+  computed: {
+    filteredWorkers() {    
+        const filteredWorkers = this.searchString === ""
+            ? this.data.img
+            : this.data.img.filter(wo => Object.values(wo).join("").toLowerCase().indexOf(this.searchString.toLowerCase()) !== -1);
+        return filteredWorkers;
+    },
+  }
 }
 </script>
-
+<style scoped>
+.search-bar {
+  width: 100px;
+}
+.search {
+  margin-top: 10px; 
+}
+</style>
