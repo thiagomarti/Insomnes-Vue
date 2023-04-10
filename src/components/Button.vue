d276e0617c10332334a34747989404fbfe98ae32
Thiago marti <octaltiticapo@gmail.com>
2023-04-10 12:42:44 -0300

    buttons component fixed

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="botones">
         <div class="icons">
-            <a href="https://wa.me/541134104508" target="_blank" class="wp"><i class="bx bxl-whatsapp"></i></a>
-            <a href="mailto:thiagomarti2008@gmail.com" target="_blank" class="gm"><i class="bx bx-envelope" aria-hidden="true"></i></a>
+            <a href="https://wa.me/541124026001" target="_blank" class="wp"><i class="bx bxl-whatsapp"></i></a>
+            <a href="mailto:ed.insomnes@gmail.com" target="_blank" class="gm"><i class="bx bx-envelope" aria-hidden="true"></i></a>
         </div>
     </div>
 </template>
