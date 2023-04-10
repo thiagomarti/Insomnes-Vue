06660bfbe9931daf295bc98b191b1a2c088d9aba
Thiago marti <octaltiticapo@gmail.com>
2023-04-10 13:01:36 -0300

    botones mas lindos

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="botones">
         <div class="icons">
-            <a href="https://wa.me/541124026001" target="_blank" class="wp"><i class="bx bxl-whatsapp"></i></a>
-            <a href="mailto:ed.insomnes@gmail.com" target="_blank" class="gm"><i class="bx bx-envelope" aria-hidden="true"></i></a>
+            <a href="https://wa.me/541124026001" target="_blank" class="wp"><i class="bx bxl-whatsapp bx-md"></i></a>
+            <a href="mailto:ed.insomnes@gmail.com" target="_blank" class="gm"><i class="bx bxl-gmail bx-md" aria-hidden="true"></i></a>
         </div>
     </div>
 </template>
